048e791dda9f6ca38688d2078f2c8d9749cd90dc
Manuel José Pulgar Anguita <manuel.pulgar@affilired.com>
2017-10-26 11:56:04 +0200

    Added top categories - exclusives - top offers - home featured views and this views to home

---
+++ application/views/top_list_featured/top_list_featured.php
@@ -0,0 +1,151 @@
+
+                  <section class="HomeTopListFeaturedBox">
+                     <div class="Box  Box-rbom ">
+                        <div class="HomeTopListColumn">
+                           <h2 class="HomeTopListColumn-largetitle">Restaurant &amp; Bar</h2>
+                           <div>
+                              <section class="TabletThreeColumns">
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/bella-italia">
+                                          <div class="left-column"><img class="lazy" src="bella-italia-logo.jpg" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/83/-C/83-Ccrlfeqs/_/rNYlmTLoepk/jYdf/bella-italia-logo.jpg" alt="Bella Italia" title="Bella Italia"></div>
+                                          <div class="right-column">
+                                             <span>Bella Italia</span>
+                                             <p>Exclusive 25% Off All Food</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/oneills">
+                                          <div class="left-column"><img class="lazy" src="oneills-logo.jpg" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/eh/p-/ehp-p54JRH8/_/rNYlmTLoepk/asMi/oneills-logo.jpg" alt="O'Neills" title="O'Neills"></div>
+                                          <div class="right-column">
+                                             <span>O'Neills</span>
+                                             <p>40% Off Food</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/harvester">
+                                          <div class="left-column"><img class="lazy" src="harvester-logo.png" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/K2/A0/K2A0UpD6xWE/_/rNYlmTLoepk/n5T6/harvester-logo.png" alt="Harvester" title="Harvester"></div>
+                                          <div class="right-column">
+                                             <span>Harvester</span>
+                                             <p>Kids Eat For £1</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div style="clear: both;"></div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/tops-pizza">
+                                          <div class="left-column"><img class="lazy" src="tops-pizza-logo.png" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/UU/B1/UUB1NALdWOc/_/rNYlmTLoepk/IZMO/tops-pizza-logo.png" alt="Tops Pizza" title="Tops Pizza"></div>
+                                          <div class="right-column">
+                                             <span>Tops Pizza</span>
+                                             <p>Feed 5 for £4 Each</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/browns-restaurant">
+                                          <div class="left-column"><img class="lazy" src="browns-restaurant-logo.jpg" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/9u/Lj/9uLj2--Tec4/_/rNYlmTLoepk/pQmC/browns-restaurant-logo.jpg" alt="Browns Restaurant" title="Browns Restaurant"></div>
+                                          <div class="right-column">
+                                             <span>Browns Restaurant</span>
+                                             <p>20% Off Your Food Bill</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/loch-fyne">
+                                          <div class="left-column"><img class="lazy" src="loch-fyne-logo.png" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/IV/aA/IVaAMczEZis/_/rNYlmTLoepk/tXDN/loch-fyne-logo.png" alt="Loch Fyne" title="Loch Fyne"></div>
+                                          <div class="right-column">
+                                             <span>Loch Fyne</span>
+                                             <p>£10 Voucher With Sign Up</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div style="clear: both;"></div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/stonehouserestaurants.co.uk">
+                                          <div class="left-column"><img class="lazy" src="stonehouse-pizza--carvery-logo.png" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/f6/tZ/f6tZW2L4Aw8/_/rNYlmTLoepk/58yH/stonehouse-pizza--carvery-logo.png" alt="Stonehouse Pizza &amp; Carvery" title="Stonehouse Pizza &amp; Carvery"></div>
+                                          <div class="right-column">
+                                             <span>Stonehouse Pizza &amp; Carvery</span>
+                                             <p>Kids Eat For £1</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/ask-italian">
+                                          <div class="left-column"><img class="lazy" src="ask-italian-logo.png" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/63/V3/63V3FEcsSFw/_/rNYlmTLoepk/tt4T/ask-italian-logo.png" alt="ASK Italian" title="ASK Italian"></div>
+                                          <div class="right-column">
+                                             <span>ASK Italian</span>
+                                             <p>25% Off Food</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/toby-carvery">
+                                          <div class="left-column"><img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/bR/Uv/bRUv9i.LLn8/_/rNYlmTLoepk/vnRL/toby-carvery-logo.jpg" alt="Toby Carvery" title="Toby Carvery"></div>
+                                          <div class="right-column">
+                                             <span>Toby Carvery</span>
+                                             <p>Kids Eat For £1</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div style="clear: both;"></div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/prezzo">
+                                          <div class="left-column"><img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/VQ/t7/VQt7AjVsTDM/_/rNYlmTLoepk/Ft9S/prezzo-logo.png" alt="Prezzo" title="Prezzo"></div>
+                                          <div class="right-column">
+                                             <span>Prezzo</span>
+                                             <p>30% Off Food</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/planet-hollywood-london">
+                                          <div class="left-column"><img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/a3/3I/a33IfDckQj0/_/rNYlmTLoepk/dsog/planet-hollywood-london-logo.png" alt="Planet Hollywood London" title="Planet Hollywood London"></div>
+                                          <div class="right-column">
+                                             <span>Planet Hollywood London</span>
+                                             <p>Exclusive 20% Off Food</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div class="TabletThreeColumns-item">
+                                    <article class="HomeOffer clearfix">
+                                       <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/burger-king">
+                                          <div class="left-column"><img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="https://web.archive.org/web/20170823083203im_/https://mvp.tribesgds.com/dyn/aE/uW/aEuWpWFyUAg/_/rNYlmTLoepk/WlR7/burger-king-logo.png" alt="Burger King" title="Burger King"></div>
+                                          <div class="right-column">
+                                             <span>Burger King</span>
+                                             <p>Bacon Double Cheeseburger and Small Fries Only £1.99</p>
+                                          </div>
+                                       </a>
+                                    </article>
+                                 </div>
+                                 <div style="clear: both;"></div>
+                                 <div style="clear: both;"></div>
+                              </section>
+                              <a href="https://web.archive.org/web/20170823083203/https://www.myvouchercodes.co.uk/restaurant-vouchers" class="ButtonLink ButtonLink-topcorner"><span>View All Restaurant &amp; Bar</span></a>
+                           </div>
+                        </div>
+                        <div style="clear: both;"></div>
+                     </div>
+                  </section>
